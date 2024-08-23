36679c1833c2d18988b30d1df3d2721ef872451c
Sanjana <sanjana.sn.07@gmail.com>
2024-08-23 11:46:11 -0700

    Added API endpoints

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+from .views import MenuView, BookingView
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingView)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/',views.MenuView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/',include(router.urls)),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from .serializers import menuSerializer, bookingSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics,viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking
+from .serializers import bookingSerializer, menuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class BookingView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    
+class MenuView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class =menuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
